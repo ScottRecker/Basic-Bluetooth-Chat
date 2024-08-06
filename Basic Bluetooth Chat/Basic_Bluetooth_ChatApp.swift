0e8d2cc2e06502c48347a9ef6bf498601ed6750b
Scott Recker <scott.a.recker@gmail.com>
2024-08-05 21:09:07 -0500

    Initial Commit

---
+++ Basic Bluetooth Chat/Basic_Bluetooth_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Bluetooth_ChatApp.swift
+//  Basic Bluetooth Chat
+//
+//  Created by Scott Recker on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Basic_Bluetooth_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
